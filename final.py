1063226ae2f581b072ce7daa4ded21557c72a53d
Asli Kimya <akimya@stanford.edu>
2016-04-21 14:43:04 -0700

    cleaned up code, deleted unused files

--- final.py
+++ final.py
@@ -1,21 +1,14 @@
-from twython import Twython
 from twython import TwythonStreamer
-import json
-import pprint
-from flask import Flask, jsonify
-from flask import render_template, request, url_for, redirect
-import threading
+from flask import Flask, jsonify, render_template, request
 from multiprocessing import Process, Queue
-import time
+import json
 
 MAX_SIZE = 200
 app = Flask(__name__)
 pics_arr = []
 counter = int(0)
 query = ""
-searchThread = []
-streamer = None
-disconnectCounter = int(0)
+allProcesses = []
 queue = Queue()
 # Extract the user credentials
 creds = json.load(open('config.json'))
@@ -24,29 +17,13 @@ creds = json.load(open('config.json'))
 @app.route('/', methods = ['POST', 'GET'])
 def render_images():
     if request.method == 'POST':
-        # if streamer != None:
-        #     streamer.disconnect()
-
-        for thread in searchThread:
-            print thread 
-            thread.terminate()
-
-            
+        for process in allProcesses:
+            process.terminate()    
         global query
         query = request.form['query-input']
-
-        # print "updated query: "+ query
-        # global searchThread
-        # print "searchThread:" 
-        # print searchThread
-        # print " ".join((''.join(elems) for elems in searchThread))
-        # print "creating new searchhread"
         stream = Process(target = streamTwitter, args=(query,))
         stream.start()
-        searchThread.append(stream)
-        print "thread after return"
-        print searchThread
-
+        allProcesses.append(stream)
     return render_template('jinjadisplay.html', mylist=pics_arr)
 
 @app.route('/data', methods = ['GET'])
@@ -63,30 +40,15 @@ class TweetStreamer(TwythonStreamer):
                 if 'media_url' in data['entities']['media'][0]:
                     url = data['entities']['media'][0]['media_url']
                     global counter
-                    print str(counter)+" "+url
-                    # if (disconnectCounter == 5):
-                    #     streamer.disconnect()
-                    #     print "DISCONNECTED"
-                    #     disconnectCounter = 0
-                    #     global searchThread
-                    #     print "searchThread:"
-                    #     print searchThread
-                    #     global query
-                    #     print "current query: "+query
-                    #     searchThread.append(Thread(target = streamTwitter(query)))
-                    #     print "after appending in success"
-                    #     return
-
                     if (counter == MAX_SIZE): 
                         pics_arr.pop([0])
                         counter-=1
                     if (url not in pics_arr):
+                        print str(counter)+" "+query+" "+url
                         queue.put(url)
                         pics_arr.append(url)
                         counter+=1
-                        global disconnectCounter
-                        disconnectCounter+=1
-                        
+
     def on_error(self, status_code, data):
         print status_code
         self.disconnect()
